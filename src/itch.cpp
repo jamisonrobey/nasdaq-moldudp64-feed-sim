c56ff04d82346a54b3c5b7f7eb0e1551b61b1d38
jamisonrobey <jamisonrobey@gmail.com>
2025-10-12 08:02:43 +1000

    add: simpler cmake for tests

--- src/itch.cpp
+++ src/itch.cpp
@@ -7,6 +7,7 @@ std::chrono::nanoseconds Itch::extract_timestamp(const std::uint8_t* bytes)
 {
     std::uint64_t timestamp{0};
 
+    // read 6-byte BE and convert to uint64_t LE
     for (std::size_t i = 0; i < Itch::timestamp_size; ++i)
     {
         timestamp = (timestamp << 8) | bytes[i];

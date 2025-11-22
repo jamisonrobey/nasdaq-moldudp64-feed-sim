6b9eaaf30f947326d7e3cb8f4131a2bf74517065
jamisonrobey <jamisonrobey@gmail.com>
2025-11-22 16:29:41 +1000

    optimization: profiled and seek was taking lots of overhead, moving to header so it can be inlined reduced run time and instruction count significantly

--- src/itch.h
+++ src/itch.h
@@ -3,6 +3,10 @@
 
 #include <cstddef>
 #include <chrono>
+#include <stdexcept>
+#include <cassert>
+#include <cstring>
+#include <netdb.h>
 
 // https://www.nasdaqtrader.com/content/technicalsupport/specifications/dataproducts/NQTVITCHSpecification.pdf
 namespace Itch
@@ -26,10 +30,52 @@ struct MessageView
     std::chrono::nanoseconds timestamp;
 };
 
-std::optional<std::span<const std::byte>> seek_next_message(std::span<const std::byte> file, std::size_t file_pos);
+inline std::optional<std::span<const std::byte>> seek_next_message(std::span<const std::byte> file, std::size_t file_pos)
+{
+    if (file_pos + len_prefix_size > file.size())
+    {
+        return std::nullopt;
+    }
+
+    std::uint16_t len_prefix{};
+    std::memcpy(&len_prefix, &file[file_pos], len_prefix_size);
+    len_prefix = ntohs(len_prefix);
+
+    const std::size_t total_msg_len = len_prefix_size + len_prefix;
+
+    [[unlikely]]
+    if (file_pos + total_msg_len > file.size())
+    {
+        throw std::runtime_error("ITCH message exceeds file size");
+    }
+
+    return std::span{file.subspan(file_pos, total_msg_len)};
+}
 
 // // reads 6-byte big-endian timestamp as u64 then returns as chrono::nanoseconds
-std::chrono::nanoseconds extract_timestamp(std::span<const std::byte> bytes);
+inline std::chrono::nanoseconds extract_timestamp(std::span<const std::byte> bytes)
+{
+    if (bytes.size() < timestamp_size)
+    {
+
+        throw std::invalid_argument("Too small to contain a valid timestamp");
+    }
+
+    std::uint64_t timestamp = 0;
+    for (std::size_t i = 0; i < timestamp_size; ++i)
+    {
+        timestamp = (timestamp << 8) | std::to_integer<std::uint64_t>(bytes[i]);
+    }
+
+    const auto timestamp_duration = std::chrono::nanoseconds{timestamp};
+    if (timestamp_duration >= max_timestamp)
+    {
+        throw std::out_of_range("Timestamp exceeds 24 hours");
+    }
+
+    return timestamp_duration;
+}
+
 }
 
 #endif

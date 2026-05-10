f8170f6b949bbbf639c5f53f13443d11281ffc9e
jamisonrobey <jamisonrobey@gmail.com>
2026-05-10 12:04:01 +1000

    Add mold/types.h

---
+++ src/mold/types.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <cstdint>
+#include <array>
+
+namespace mold
+{
+    using Session = std::array<char, 10>;
+    using SequenceNumber = std::uint64_t;
+    using MessageCount = std::uint16_t;
+
+    inline constexpr std::size_t header_length{sizeof(Session) + sizeof(SequenceNumber) + sizeof(MessageCount)};
+
+    using LengthPrefix = std::uint16_t;
+};
